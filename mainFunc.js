e8dd19c774eddb35c1c5eb93574f3dc974fb5af2
Md Sakib <65080433+mdsakib-11@users.noreply.github.com>
2021-06-02 21:54:50 +0600

    ClickshowImage
    ClickshowImage

---
+++ mainFunc.js
@@ -0,0 +1,11 @@
+let buttonOne = document.getElementsByClassName("buttonOne")[0];
+let buttonTwo = document.getElementsByClassName("buttonTwo")[0];
+let imageTag = document.querySelector("#showImg");
+
+function clientOne(){
+    imageTag.src="https://i.ibb.co/NFCgB1x/client.jpg";
+}
+
+function clientTwo(){
+    imageTag.src="https://i.ibb.co/tP1vJRT/client-Two.webp";
+}
